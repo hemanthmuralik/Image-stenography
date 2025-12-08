32e5855deece17374bada38c8cb73a4bc9ecc6fe
Hemanth murali k <hemanthmuralikattur@gmail.com>
2025-12-08 17:56:49 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 int main() {
+    void print_usage(void)
+{
+    printf("Image Steganography Tool (LSB-based)\n");
+    printf("Usage:\n");
+    printf("  Encode: ./stego -e <cover.bmp> <secret_file> <output.bmp>\n");
+    printf("  Decode: ./stego -d <stego.bmp>\n");
+}
+
     printf("Steganography Tool Placeholder\n");
     return 0;
 }
